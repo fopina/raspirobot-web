b7242212f07ec799e805418d9bbdac7519710e23
Filipe Pina <fopina@skmobi.com>
2014-07-04 11:57:05 +0100

    added bootstrap-switch from http://www.bootstrap-switch.org

--- raspirobot_web.py
+++ raspirobot_web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify, request
 from raspirobotboard import RaspiRobot
 
 app = Flask(__name__)
@@ -34,9 +34,23 @@ def stop():
 	rr.stop()
 	return jsonify(status = '0')
 
+@app.route('/led1', methods=['POST'])
+def led1():
+	if request.json:
+		state = 0
+		try:
+			state = int(request.json.get('state', 0))
+		except:
+			pass
+
+		rr.set_led1(state)
+
+	return jsonify(status = '0')
+
+
 # init
 rr = RaspiRobot()
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port = 5001)
+	app.run(host='0.0.0.0', port = 5001, debug = True)
