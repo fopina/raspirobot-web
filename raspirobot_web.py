e6d47b931cad611e7a6657671fbe0107fd27fec1
Filipe Pina <fopina@skmobi.com>
2014-07-04 12:50:00 +0100

    added control over ledX, ocX and swX

--- raspirobot_web.py
+++ raspirobot_web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template,jsonify, request
+from flask import Flask, render_template,jsonify, request, abort
 from raspirobotboard import RaspiRobot
 
 app = Flask(__name__)
@@ -34,19 +34,59 @@ def stop():
 	rr.stop()
 	return jsonify(status = '0')
 
-@app.route('/led1', methods=['POST'])
-def led1():
+@app.route('/output', methods=['POST'])
+def switch():
+	if not request.json:
+		abort(400)
+
+	switch_name = request.json.get('switch')
+
+	if type(switch_name) is not unicode:
+		abort(400)
+
+	if switch_name not in ['led1', 'led2', 'oc1', 'oc2']:
+		abort (400)
+
+	if 'state' in request.json:
+		state = request.json['state']
+
+		if type(state) is not bool:
+			abort(400)
+
+		method = getattr(rr, 'set_' + switch_name)
+		method(state)
+
+	return jsonify(state = state)
+
+@app.route('/output/<switch_name>', methods=['GET','POST'])
+def output(switch_name):
+	if switch_name not in [ 'led1', 'led2', 'oc1', 'oc2' ]:
+		abort (400)
+
+	state = False
+
 	if request.json:
-		state = 0
-		try:
-			state = int(request.json.get('state', 0))
-		except:
-			pass
+		if 'state' not in request.json:
+			abort(400)
 
-		rr.set_led1(state)
+		state = request.json['state']
 
-	return jsonify(status = '0')
+		if type(state) is not bool:
+			abort(400)
 
+		method = getattr(rr, 'set_' + switch_name)
+		method(state)
+
+	return jsonify(state = state)
+
+@app.route('/input/<switch_name>', methods=['GET','POST'])
+def input(switch_name):
+	if switch_name not in [ 'sw1', 'sw2' ]:
+		abort (400)
+	
+	state = getattr(rr, switch_name + '_closed')()
+
+	return jsonify(closed = state)
 
 # init
 rr = RaspiRobot()
